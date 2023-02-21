4f52fc6baca98a7694dde167b5c41989eb90c3cd
powerjett <jindai.pjt@raycloud.com>
2023-02-21 21:10:08 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Stage, Layer, Text } from 'react-konva';
+const text = 'react-eimele 示例\n第二段文字q'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text
+          left={30}
+          top={30}
+          fill={'red'}
+          draggable={true}
+          text={text}
+          fontSize={60}>
+        </Text>
+      </Layer>
+    </Stage>
   );
 }
 
